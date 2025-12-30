fb6e8d03046f2d24459c3aed42b1b15920aa3101
kaspernj <kasper@diestoeckels.de>
2025-12-30 12:24:36 +0000

    Add shared system test helper and new finder/interact specs

--- spec/system-test.spec.js
+++ spec/system-test.spec.js
@@ -1,58 +1,12 @@
 // @ts-check
 
-import wait from "awaitery/build/wait.js"
-
 import SystemTest from "../src/system-test.js"
-import DummyHttpServerEnvironment from "./support/dummy-http-server.js"
+import SystemTestHelper from "./support/system-test-helper.js"
 
-const debug = process.env.SYSTEM_TEST_DEBUG === "true"
-const debugLog = (...args) => { if (debug) console.log(...args) }
-const dummyHttpServerEnvironment = new DummyHttpServerEnvironment()
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000
+const systemTestHelper = new SystemTestHelper()
+systemTestHelper.installJasmineHooks()
 
 describe("System test", () => {
-  /** @type {SystemTest | undefined} */
-  let systemTest
-
-  beforeAll(async () => {
-    debugLog("[system-test] beforeAll: starting dummy HTTP env")
-    try {
-      await dummyHttpServerEnvironment.start()
-      await wait(1000)
-
-      debugLog("[system-test] beforeAll: creating SystemTest")
-      systemTest = SystemTest.current({
-        debug,
-        host: "127.0.0.1",
-        port: 6001,
-        httpHost: "0.0.0.0",
-        httpPort: 6001,
-        errorFilter: (error) => {
-          if (typeof error?.value?.[0] === "string" && error.value[0].includes("Uncaught Error: Minified React error #418; visit")) return false
-          return true
-        }
-      })
-      debugLog("[system-test] beforeAll: starting SystemTest")
-      await systemTest.start()
-      debugLog("[system-test] beforeAll: SystemTest started")
-    } catch (error) {
-      console.error("[system-test] beforeAll error", error)
-      throw error
-    }
-  })
-
-  afterAll(async () => {
-    debugLog("[system-test] afterAll: stopping SystemTest and dummy HTTP env")
-    try {
-      await systemTest?.stop()
-      await dummyHttpServerEnvironment.stop()
-      debugLog("[system-test] afterAll: teardown complete")
-    } catch (error) {
-      console.error("[system-test] afterAll error", error)
-      throw error
-    }
-  })
-
   it("shows the welcome text on the front page", async () => {
     await SystemTest.run(async (runningSystemTest) => {
       await runningSystemTest.visit("/")
