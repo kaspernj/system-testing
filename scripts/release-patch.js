4131f33403c07313b4848de102ae4c01a5a29b99
kaspernj <kasper@diestoeckels.de>
2026-03-23 10:08:33 +0000

    Improved the release:patch script

--- scripts/release-patch.js
+++ scripts/release-patch.js
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
 import {execFile, spawn} from "node:child_process"
 import {promisify} from "node:util"
+import process from "node:process"
 
 const execFileAsync = promisify(execFile)
 
-const run = (command, args = []) => new Promise((resolve, reject) => {
+/**
+ * @param {string} command
+ * @param {string[]} [args]
+ * @returns {Promise<void>}
+ */
+export const run = (command, args = []) => new Promise((resolve, reject) => {
   const child = spawn(command, args, {stdio: "inherit"})
 
   child.on("error", reject)
@@ -17,7 +23,8 @@ const run = (command, args = []) => new Promise((resolve, reject) => {
   })
 })
 
-const isNpmLoggedIn = async () => {
+/** @returns {Promise<boolean>} */
+export const isNpmLoggedIn = async () => {
   try {
     await execFileAsync("npm", ["whoami"])
     return true
@@ -26,17 +33,52 @@ const isNpmLoggedIn = async () => {
   }
 }
 
-if (!await isNpmLoggedIn()) {
-  await run("npm", ["login"])
+/** @returns {Promise<void>} */
+export const updateLocalMasterFromOrigin = async () => {
+  await run("git", ["fetch", "origin"])
+  await run("git", ["checkout", "master"])
+  await run("git", ["merge", "origin/master"])
 }
 
-await run("npm", ["version", "patch", "--no-git-tag-version"])
-await run("npm", ["install"])
-await run("npm", ["run", "all-checks"])
+/** @returns {Promise<string>} */
+export const currentVersion = async () => {
+  const {stdout: versionStdout} = await execFileAsync("npm", ["pkg", "get", "version"], {encoding: "utf8"})
+  return versionStdout.trim().replace(/"/g, "")
+}
 
-const {stdout: versionStdout} = await execFileAsync("npm", ["pkg", "get", "version"], {encoding: "utf8"})
-const version = versionStdout.trim().replace(/"/g, "")
-await run("git", ["add", "package.json", "package-lock.json"])
-await run("git", ["commit", "-m", `Release v${version}`])
-await run("git", ["push", "origin", "master"])
-await run("npm", ["publish"])
+/**
+ * @param {{
+ *   currentVersion?: typeof currentVersion
+ *   isNpmLoggedIn?: typeof isNpmLoggedIn
+ *   run?: typeof run
+ *   updateLocalMasterFromOrigin?: typeof updateLocalMasterFromOrigin
+ * }} [dependencies]
+ * @returns {Promise<void>}
+ */
+export const releasePatch = async (dependencies = {}) => {
+  const currentVersionDependency = dependencies.currentVersion || currentVersion
+  const isNpmLoggedInDependency = dependencies.isNpmLoggedIn || isNpmLoggedIn
+  const runDependency = dependencies.run || run
+  const updateLocalMasterFromOriginDependency = dependencies.updateLocalMasterFromOrigin || updateLocalMasterFromOrigin
+
+  await updateLocalMasterFromOriginDependency()
+
+  if (!await isNpmLoggedInDependency()) {
+    await runDependency("npm", ["login"])
+  }
+
+  await runDependency("npm", ["version", "patch", "--no-git-tag-version"])
+  await runDependency("npm", ["install"])
+  await runDependency("npm", ["run", "all-checks"])
+
+  const version = await currentVersionDependency()
+
+  await runDependency("git", ["add", "package.json", "package-lock.json"])
+  await runDependency("git", ["commit", "-m", `Release v${version}`])
+  await runDependency("git", ["push", "origin", "master"])
+  await runDependency("npm", ["publish"])
+}
+
+if (process.argv[1] && import.meta.url === new URL(`file://${process.argv[1]}`).href) {
+  await releasePatch()
+}
